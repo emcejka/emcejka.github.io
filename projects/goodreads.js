af9fc908fe77259c86f15a7929c3661e91fcf658
emcejka <emily.cejka@gmail.com>
2023-07-16 21:31:26 -0400

    Added numbers to the photos, making carousel work

--- projects/goodreads.js
+++ projects/goodreads.js
@@ -1,24 +1,33 @@
-const carouselSlide = document.querySelector(".carousel-slide");
-const images = document.querySelectorAll(".carousel-slide img");
+document.addEventListener("DOMContentLoaded", function () {
+  const carouselSlide = document.querySelector(".carousel-slide");
+  const images = document.querySelectorAll(".carousel-slide img");
 
-let counter = 0;
-const slideWidth = images[0].clientWidth;
+  let counter = 0;
+  let slideWidth = 0;
 
-function changeSlide(direction) {
-  counter += direction;
-  if (counter < 0) {
-    counter = images.length - 1;
-  } else if (counter >= images.length) {
-    counter = 0;
+  function initCarousel() {
+    slideWidth = images[0].clientWidth;
+    carouselSlide.style.transform = `translateX(-${slideWidth * counter}px)`;
   }
 
-  carouselSlide.style.transform = `translateX(-${slideWidth * counter}px)`;
-}
+  function changeSlide(direction) {
+    counter += direction;
+    if (counter < 0) {
+      counter = images.length - 1;
+    } else if (counter >= images.length) {
+      counter = 0;
+    }
 
-document.querySelector(".prev-btn").addEventListener("click", () => {
-  changeSlide(-1);
-});
+    carouselSlide.style.transform = `translateX(-${slideWidth * counter}px)`;
+  }
 
-document.querySelector(".next-btn").addEventListener("click", () => {
-  changeSlide(1);
+  document.querySelector(".prev-btn").addEventListener("click", () => {
+    changeSlide(-1);
+  });
+
+  document.querySelector(".next-btn").addEventListener("click", () => {
+    changeSlide(1);
+  });
+
+  initCarousel();
 });
